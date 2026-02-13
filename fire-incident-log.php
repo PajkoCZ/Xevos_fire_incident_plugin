81452f71c45479b3b3cd4912b82ba60d2358fd87
PM-Production <it.prislusenstvi@gmail.com>
2026-02-13 13:17:26 +0100

    Inicializace pluginu + základní kostra pro definování

---
+++ fire-incident-log.php
@@ -0,0 +1,14 @@
+<?php
+/** 
+ * Plugin Name: Výjezdy hasičů
+ * Descrtiption: Plugin pro zaznamování výjezdů hasičů
+ * Version: 1.0
+ * Autor: Pavel Mančík
+*/
+
+if (!defined('ABSPATH')) exit;
+
+define( 'FIL_PATH', plugin_dir_patch(__FILE__) );
+define( 'FIL_URL', plugin_dir_url(__FILE__) );
+
+?>
